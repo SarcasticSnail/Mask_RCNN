d811d7a917a1d47280c17f8708e9eed510dd67e9
SarcasticSnail <54252089+SarcasticSnail@users.noreply.github.com>
2021-12-02 22:42:23 -0500

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -19,7 +19,7 @@ import numpy as np
 import tensorflow as tf
 import keras
 import keras.backend as K
-import keras.layers as KL
+import keras.layers.topology as KL
 import keras.engine as KE
 import keras.models as KM
 
